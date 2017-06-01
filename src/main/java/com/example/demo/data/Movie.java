b2d9a9467ab8ddc1f71e2b33f0bbae3bc72289ae
Covenant <CovenantChukwudi@gmail.com>
2017-06-01 23:59:31 +0100

    created the Movie entity class

---
+++ src/main/java/com/example/demo/data/Movie.java
@@ -0,0 +1,33 @@
+package com.example.demo.data;
+
+public class Movie {
+	
+	private Long id;
+	private String movieName;
+	private String movieDescription;
+	
+	public Movie(){
+		
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getMovieName() {
+		return movieName;
+	}
+	public void setMovieName(String movieName) {
+		this.movieName = movieName;
+	}
+	public String getMovieDescription() {
+		return movieDescription;
+	}
+	public void setMovieDescription(String movieDescription) {
+		this.movieDescription = movieDescription;
+	}
+	
+	
+}
