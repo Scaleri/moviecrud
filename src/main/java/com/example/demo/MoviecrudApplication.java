d2544a2abf39ee563ce3913f69b43ce581a92544
Covenant <CovenantChukwudi@gmail.com>
2017-06-01 23:13:14 +0100

    created the basic packages

---
+++ src/main/java/com/example/demo/MoviecrudApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviecrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviecrudApplication.class, args);
+	}
+}
